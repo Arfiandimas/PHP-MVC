2468dedf7acea795d1561db86c0a71235bd8eb00
Arfian Dimas Andi Permana <arfiandimas1929@gmail.com>
2020-04-12 23:17:04 +0700

    Model

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container">
 	<h1>About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.JPG" alt="Arfian Dimas Andi Permana" width="200" class="shadow">
 	<p>Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
